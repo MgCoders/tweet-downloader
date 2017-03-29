8b08639c634d49f60f1e272619591ee64afabc8b
raul <raul@mgcoders.uy>
2017-03-29 18:41:44 -0300

    mongo

--- libs/_key_mongo.py
+++ libs/_key_mongo.py
@@ -1,10 +1,5 @@
-#--------------------------------------------------------------------------------
-# These tokens are needed for user authentication.
-# Credentials can be generates via Twitter's Application Management:
-#	https://apps.twitter.com/app/new
-#--------------------------------------------------------------------------------
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+
+dbcoll = "imported-data"
+dbdb = "krypton"
+mongostring=
